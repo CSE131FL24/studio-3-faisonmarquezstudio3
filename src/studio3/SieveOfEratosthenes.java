8385f234efd366f63ce82004095d00cde73e541c
kipfaison <kipfaison@localhost>
2024-09-19 11:19:40 -0500

    studio3

---
+++ src/studio3/SieveOfEratosthenes.java
@@ -0,0 +1,48 @@
+package studio3;
+
+public class SieveOfEratosthenes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 40;
+		
+		
+		int[] numbers = new int[n-1];
+
+		boolean[] primenumbers = new boolean[n-1]; 
+		
+		for (int i = 0; i+1 < n; i++) {
+			numbers[i] = i+2;
+			primenumbers[i] = true;
+			
+			for 
+			
+			
+			
+			
+		System.out.print(numbers[n])
+		
+		;	
+			
+			
+		}
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
